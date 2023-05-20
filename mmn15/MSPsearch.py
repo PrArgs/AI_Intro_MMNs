53f42a87288ee815e8f2b644b85b9378f17153ca
Roi Argaman <zebroi.arg@gmail.com>
2023-05-21 01:54:03 +0300

    finish documenting

--- mmn15/MSPsearch.py
+++ mmn15/MSPsearch.py
@@ -32,43 +32,54 @@ class MissionariesCannibalsProblem():
     GOAL_STATE = [3,3,0,0,LEFTBANCK]
     START_STATE = [0,0,3,3,RIGHTBANCK]
 
-    def __init__(self, state =START_STATE, action = ""):
+    """Constructor of MissionariesCannibalsProblem class"""
+    def __init__(self, state =START_STATE, action = "", goal = GOAL_STATE):
         #list of 5 elements M in left bank, K in left bank, M in right bank, K in right bank, 5 boat position
         self.state = state
         self.Action = action
+        self.goal = goal
 
+    """Make MissionariesCannibalsProblem object get indexable""" 
     def __getitem__(self, index):
         return self.state[index]
 
+    """Make MissionariesCannibalsProblem object set indexable"""
     def __setitem__(self, index, value):
         self.state[index] = value
 
+    """Get the action that led to this state"""
     def getAction(self):  
         return (self.Action)
     
+    """Make MissionariesCannibalsProblem object hashable"""
     def __hash__(self):
         value = 0
         for i in range(0,5):
             value +=np.power(2,self.state[i]*i)
         return (int(value))
     
+    """Make MissionariesCannibalsProblem object printable"""
     def __str__(self):
         return str(self.state)
     
+    """Make MissionariesCannibalsProblem object copyable"""
     def copy(self):
         return MissionariesCannibalsProblem(self.state.copy(), self.Action)
     
+    """Make MissionariesCannibalsProblem object comparable"""
     def __eq__(self, other):
         return self.__hash__() == other.__hash__()
     
-    def isGoalState(self ,goal = GOAL_STATE):
-        goal = MissionariesCannibalsProblem(goal)
+    """Check if the state is the goal state"""
+    def isGoalState(self):
+        goal = MissionariesCannibalsProblem(self.goal)
         return self.__eq__(goal)
     
+    """Get the start state"""
     def getStartState(self):
         return self.copy()    
             
-    # check if the state is legal
+    """Check that the state is legal"""
     def isLegal(self):
         
         #chec that there are'nt any missionaries outnumbered by kannibals on any bank
@@ -78,21 +89,16 @@ class MissionariesCannibalsProblem():
         #check if one of the inedexes is negative or bigger than 3
         for tmp in self.state:
             if tmp < 0 or tmp > 3:
-                return False            
+                return False 
         
-        
-        ##############################
-        ##############################
-        #print(self.getAction(), "\n\n")
-        ##############################
-        ##############################
         return True
     
-    #Check if object is instance of MissionariesCannibalsProblem for debugging reasons.
+    """Check if object is instance of MissionariesCannibalsProblem for debugging reasons."""
     def __isinstance__(obj, MissionariesCannibalsProblem):
         return isinstance(obj, MissionariesCannibalsProblem)
     
-    #drop passengers on the other bank upone arrival
+    """drop passengers on the other bank upone arrival"""
+    
     def dropPasnngares(self):
         result = self.state.copy()
         KanibalsOnBoat =  3- (result[KLB] + result[KRB])
@@ -114,60 +120,11 @@ class MissionariesCannibalsProblem():
             return MissionariesCannibalsProblem(result , state) 
         else:
             raise Exception("Error in dropPasnngares you can't drop passengers in the river")
-           
-    """def crossRiver(self):
-        successors = []
-        # Boat came from left bank
-        if self.state[LBP] == LEFTBANCK:
-            temp = self.state.copy()
-            temp[BP] = RIGHTBANCK
-            temp[LBP] = RIVER
-            successors.append(MissionariesCannibalsProblem(temp))
-            return successors
-        # Boat came from right bank
-        else:
-            temp = self.state.copy()
-            temp[BP] = LEFTBANCK
-            temp[LBP] = RIVER
-            successors.append(MissionariesCannibalsProblem(temp))
-            return successors
-    """
-        
-    """def LeavLeftBank(self):
-        successors = []
-        droped = MissionariesCannibalsProblem(self.dropPasnngares())
-        if droped.isGoalState():
-            successors.append(droped)
-           
-        #put people on boat
-        for i in range(0,3):
-            for j in range(0,3):
-                if i+j <3 and i+j > 0:
-                    temp = droped.copy()
-                    temp[MLB] -= i
-                    temp[KLB] -= j
-                    temp[BP] = RIGHTBANCK
-                    action = "Takes " + str(i) + " Missionaries and " + str(j) + " Kannibals from left bank to right bank"                    
-                    successors.append(MissionariesCannibalsProblem(temp , action))
-        return successors                 
-    
-    def LeavRightBank(self):
-        successors = []
-        droped = MissionariesCannibalsProblem(self.dropPasnngares())
-        for i in range(0,3):
-            for j in range(0,3):
-                if i+j <3 and i+j > 0:
-                    temp = droped.copy()
-                    temp[MRB] -= i
-                    temp[KRB] -= j
-                    temp[BP] = LEFTBANCK
-                    action = "Takes " + str(i) + " Missionaries and " + str(j) + " Kannibals from right bank to left bank"
-                    successors.append(MissionariesCannibalsProblem(temp, action))
-        return successors"""
-    
+                
+    """Get the successors of the state"""
     def getSuccessors(self):
         successors = []
-        #drop passengers on the other bank upone arrival
+        #drop passengers on the bank upone arrival
         droped = self.dropPasnngares()
         if droped.isGoalState():
                 successors.append(droped)
@@ -189,7 +146,7 @@ class MissionariesCannibalsProblem():
             toBank = "left"
             
 
-         #put people on boat
+        #Board people on boat
         for mission in range(0,3):
             for kanibal in range(0,3):
                 if mission+kanibal <3 and mission+kanibal > 0:
@@ -200,29 +157,30 @@ class MissionariesCannibalsProblem():
                     action = "Takes " + str(mission) + " Missionaries and " + str(kanibal) + " Kannibals from " + fromBank + " bank to " + toBank + " bank"
                     temp.Action = action                    
                     successors.append(temp)
-                    
-        # #make sure we don't explore states that will lead to the state we are currently at.
-        # chekMuteMove = temp.getSuccessors()
-        # if len(chekMuteMove) == 1 and self.__eq__(chekMuteMove[0]):
-        #     successors.remove(temp)
-        #filter the legal successors
-        successors = filter(MissionariesCannibalsProblem.isLegal, successors)
-        #Make sure each successor appears only once in the list using set
-        successors = list(set(successors))
 
+
+        #Debugging in case of error
         for node in successors:
             if not isinstance(node, MissionariesCannibalsProblem):
                 print("Error in the type of the node") 
-                exit(1)
+                exit(1)                    
+        
+        #filter the legal successors
+        successors = filter(MissionariesCannibalsProblem.isLegal, successors)
+
+        #Make sure each successor appears only once in the list using set
+        successors = list(set(successors))       
         
         return successors
+    
 
-def backtrcking(node,startState, fatherSunDict , howmayNodes, algorithm = ""):
-    """
+"""
     This function is used to backtrack from the goal state to the start state
     and return the list of actions that lead to the goal state from the start state
     by using the fatherSunDict dictionary that contains the parent of each node
     """
+def backtrcking(node,startState, fatherSunDict , howmayNodes, algorithm = ""):
+    
     result = []  
     while not startState.__eq__(node):        
         result.append(node.getAction())
@@ -231,7 +189,10 @@ def backtrcking(node,startState, fatherSunDict , howmayNodes, algorithm = ""):
     for node in result:
         print(node)
     print("Over all " ,algorithm, " have explored ",howmayNodes, " nodes and the route is of length:", len(result) )
+
+    #Not in use for now but it can be used to save the result if needed
     return result
+
 
 # BFS
 def breadthFirstSearch(problem):
@@ -280,6 +241,8 @@ def aStarSearch(prblem):
 
 
 def main():
+
+    
 
     problem = MissionariesCannibalsProblem()
     # ask the user to enter the algorithm he wants to use as a list of numbers
--- mmn15/tempCodeRunnerFile.py
+++ mmn15/tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-#make sure we don't explore states that will lead to the state we are currently at.
-        # chekMuteMove = temp.getSuccessors()
-        # if len(chekMuteMove) == 1 and self.__eq__(chekMuteMove[0]):
-        #     successors.remove(temp)
+state
